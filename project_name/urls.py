aa3e38301a43ea8e0340a5d2f2b2ebdfd870fc0d
Brian Rosner <brosner@gmail.com>
2013-04-08 12:44:17 -0600

    updated homepage URL to use TemplateView

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,17 +1,15 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
-
     url(r"^account/", include("account.urls")),
 )
 

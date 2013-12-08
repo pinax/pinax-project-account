75978ab4270e139822204be2fecd55b5b8309efd
Patrick Altman <paltman@gmail.com>
2013-12-08 17:53:49 -0600

    Move admin startup to startup.py script

--- project_name/startup.py
+++ project_name/startup.py
@@ -4,13 +4,15 @@ from django.conf import settings
 from django.utils.importlib import import_module
 from django.utils.module_loading import module_has_submodule
 
+from django.contrib import admin
+
 
 def autoload(submodules):
     for app in settings.INSTALLED_APPS:
         mod = import_module(app)
         for submodule in submodules:
             try:
-                import_module("{}.{}".format(app, submodule))
+                import_module("{0}.{1}".format(app, submodule))
             except:
                 if module_has_submodule(mod, submodule):
                     raise
@@ -18,3 +20,4 @@ def autoload(submodules):
 
 def run():
     autoload(["receivers"])
+    admin.autodiscover()

8aed1ff39b0f608098d9e51bedae19b68528518e
Patrick Altman <paltman@gmail.com>
2013-12-14 16:05:13 -0600

    Pull SITE_ID from env if available, default to 1

--- project_name/settings.py
+++ project_name/settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = "UTC"
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "en-us"
 
-SITE_ID = 1
+SITE_ID = int(os.environ.get("SITE_ID", 1))
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.

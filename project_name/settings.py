a627c2bba7deeb9181a4e1ede91902d80008b9a1
Brian Rosner <brosner@gmail.com>
2014-07-29 13:37:29 -0600

    tweaked settings to prevent Django 1.5 detection in Django 1.7

--- project_name/settings.py
+++ project_name/settings.py
@@ -3,15 +3,10 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = PACKAGE_ROOT
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = [
-    # ("Your Name", "your_email@example.com"),
-]
-
-MANAGERS = ADMINS
 
 DATABASES = {
     "default": {

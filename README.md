c208f861de4d7d26666d5d471e6ccc666122ad26
Patrick Altman <paltman@gmail.com>
2015-08-06 15:59:26 -0500

    Update structure of frontend build

--- README.md
+++ README.md
@@ -36,21 +36,14 @@ We rely on the Node Package Manager (`npm`), which comes with `node`. If on the
 Mac, you can just `brew install node`. Otherwise, you can find [documentation](https://docs.npmjs.com/getting-started/installing-node)
 on how to install it for your operating system or desired setup.
 
-If you don't already have `webpack` setup globally on your system run:
-
-```
-npm install webpack -g
-```
-
-Now run:
-
 ```
 npm install  # this will install all the dependencies in package.json
-webpack      # this will build everything according to the configuration we've provided in webpack.config.js
+npm run watch  # run a watcher
+npm run build  # just build the media
 ```
 
 #### Setting up a watcher
 
-You can run `webpack -w` in a terminal to watch the file system and rebuild just
+You can run `npm run watch` in a terminal to watch the file system and rebuild just
 what is needed when you save changes. This will work for any `less` or `js`
 changes.
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -3,19 +3,33 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-    context: __dirname + "/static/src",
+    context: path.resolve(__dirname, "src"),
     entry: {
         app: "./js/main.js"
     },
     output: {
-        path: __dirname + "/static/dist",
+        path: path.resolve(__dirname, "dist"),
         filename: "/js/site.js",
         publicPath: "/site_media/static"
     },
     module: {
         loaders: [
+            {
+                test: /\.(gif|png|ico|jpg|svg)$/,
+                include: [
+                    path.resolve(__dirname, "src/images")
+                ],
+                loader: "file-loader?name=/images/[name].[ext]"
+            },
             { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") },
-            { test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=/fonts/[name].[ext]?[hash]" },
+            {
+                test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                include: [
+                    path.resolve(__dirname, "/src/fonts"),
+                    path.resolve(__dirname, "../node_modules")
+                ],
+                loader: "file-loader?name=/fonts/[name].[ext]?[hash]"
+            },
             { test: /\.jsx?$/, loader: "babel-loader", query: {compact: false} },
         ]
     },
